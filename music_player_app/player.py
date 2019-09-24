b7ee261a27ee4b26050aaf5231def2767d725474
dodge-ttu <will@iamdodge.us>
2019-09-23 23:08:50 -0500

    update

--- music_player_app/player.py
+++ music_player_app/player.py
@@ -1,5 +1,7 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QSize
 
 
 class Player(QWidget):
@@ -11,7 +13,42 @@ class Player(QWidget):
         self.show()
 
     def UI(self):
-        pass
+        self.widgets()
+        self.layouts()
+
+    def widgets(self):
+        ##### PROGRESS BAR #####
+        self.progressBar=QProgressBar()
+        ##### BUTTONS #####
+        self.addButton=QToolButton()
+        self.addButton.setIcon(QIcon("./icons/add.png"))
+        self.addButton.setIconSize(QSize(64,64))
+
+    def layouts(self):
+        ##### LAYOUTS #####
+        self.mainLayout=QVBoxLayout()
+        self.topMainLayout=QVBoxLayout()
+        self.topGroupBox=QGroupBox("Music Player")
+        self.topGroupBox.setStyleSheet('background-color:#00E19B;')
+        self.topLayout=QHBoxLayout()
+        self.middleLayout=QHBoxLayout()
+        self.bottomLayout=QVBoxLayout()
+
+        ##### ADDING WIDGETS #####
+        ##### TOP LAYOUT WIDGETS #####
+        self.topLayout.addWidget(self.progressBar)
+        ##### Middle LAYOUT WIDGETS #####
+        self.middleLayout.addWidget(self.addButton)
+
+
+
+        self.topMainLayout.addLayout(self.topLayout)
+        self.topMainLayout.addLayout(self.middleLayout)
+        self.topGroupBox.setLayout(self.topMainLayout)
+        self.mainLayout.addWidget(self.topGroupBox)
+        self.mainLayout.addLayout(self.bottomLayout)
+        self.setLayout(self.mainLayout)
+
 
 
 def main():
